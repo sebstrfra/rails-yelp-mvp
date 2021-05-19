638d41fea14ed5a3c5bc1fd036f24730559ea26c
Sebastian Strube <sebstrfra@gmail.com>
2021-05-19 14:33:28 +0200

    before coffee walk

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # verb 'path', to: 'controller#action'
+
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+
+  root to: 'restaurants#index'
+
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+
 end
